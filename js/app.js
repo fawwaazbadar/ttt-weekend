f9ded1b08310e33ff1a08ac31c25af36e4850c5d
Fawwaaz Badar <bfawwaaz@gmail.com>
2021-10-12 10:03:45 -0400

    start lab

--- js/app.js
+++ js/app.js
@@ -3,10 +3,18 @@
 
 
 /*---------------------------- Variables (state) ----------------------------*/
-
-
+let winnerMessage = ''
+let currentPlayer = "X"
+let board = 
+[
+  null, null, null,
+  null, null, null,
+  null, null, null
+]
 
 /*------------------------ Cached Element References ------------------------*/
+const messageEl = document.querySelector("#message")
+
 
 
 
@@ -15,4 +23,8 @@
 
 
 /*-------------------------------- Functions --------------------------------*/
+
+init()
+
+
 
